e0e4f26d6196ba006b2d524ddbb0291f7eaf710e
Preston Nichols <preston@auctionsoftware.com>
2021-05-11 16:16:03 -0500

    adding console log

--- client/src/Components/SearchMenu/index.js
+++ client/src/Components/SearchMenu/index.js
@@ -108,6 +108,7 @@ export default function PrimarySearchAppBar(props) {
 
   useEffect(  () => {
      Axios.get("https://i20fireworks.herokuapp.com/api/getBrands").then((res) => {
+       console.log("IT HIT!!!!", res.data)
       setBrandList(res.data);
     });
 
