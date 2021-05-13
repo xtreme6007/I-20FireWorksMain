9728ef64958c4c22df4b2cf5fdb922e654b18abf
Preston Nichols <preston@auctionsoftware.com>
2021-05-13 12:58:18 -0500

    added in validation to show admin link when im logged in

--- client/src/Components/Nav/index.js
+++ client/src/Components/Nav/index.js
@@ -1,11 +1,21 @@
-import Reactg from "react";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
 import { useAuth0 } from "@auth0/auth0-react";
+import { useState, useEffect } from "react";
+import Axios from "axios";
 
 export default function NavHeader() {
   const { loginWithRedirect, isAuthenticated, logout } = useAuth0();
+  const [logedInStatus, setLogedInStatus] = useState([])
+
+  useEffect(() => {
+    Axios.get("/api/login").then((response)=> {
+      // console.log(response)
+      if(response.data.LoggedIn == true){
+          setLogedInStatus(response.data.User[0].user_name)
+      }
+  })
+  })
 
   return (
     <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" sticky="top">
@@ -14,7 +24,7 @@ export default function NavHeader() {
       <Navbar.Collapse id="responsive-navbar-nav">
         <Nav className="mr-auto">
           <Nav.Link href="/search">Shop</Nav.Link>
-          {isAuthenticated ? (
+          {logedInStatus === "Preston_Nichols" ? (
             <Nav.Link href="/admin/postNew">Admin</Nav.Link>
           ) : null}
         </Nav>
