4f31b30fc1f870e72993e7e61221ff8058001095
Preston Nichols <preston@auctionsoftware.com>
2021-05-17 09:52:47 -0500

    added in notification for succsefull/failed logged in as well as redirect for succesfull login

--- client/src/Components/Login/index.js
+++ client/src/Components/Login/index.js
@@ -1,49 +1,101 @@
-import react from 'react';
+import react from "react";
 import { useState, useEffect, useRef } from "react";
 import Axios from "axios";
-import Container from '@material-ui/core/Container';
-import TextField from '@material-ui/core/TextField';
-import Button from '@material-ui/core/Button';
+import Container from "@material-ui/core/Container";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import Snackbar from "@material-ui/core/Snackbar";
+import MuiAlert from '@material-ui/lab/Alert';
+import { Redirect } from "react-router-dom";
 
-
-
-
-Axios.defaults.withCredentials = true
+Axios.defaults.withCredentials = true;
 
 export default function Login() {
-    const [userName, setUserName] = useState([]);
-    const [password, setPassword] = useState([]);
-    
+  const [userName, setUserName] = useState([]);
+  const [password, setPassword] = useState([]);
+  const [loginSuccess, setLoginSuccess] = useState(false);
+  const [open, setOpen] = useState(false);
 
+  useEffect(() => {
+      console.log(open)
 
-return(
-<Container>
-        <form>
-        
-        <TextField type = "text" label = "Username" onChange = {(e) => {
-            setUserName(e.target.value)
+  },[])
 
-        }}>
-        </TextField>
-        <br/>
-        <TextField type = "text" label= "Password" onChange = {(e) => {
-            setPassword(e.target.value)
+  function Alert(props) {
+    return <MuiAlert elevation={6} variant="filled" {...props} />;
+  }
 
-        }}></TextField>
-        <br/>
+  const handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
 
-        
-        <Button variant="outlined" color="primary" onClick = {() => {
-             Axios.post("/api/login", {
+    setOpen(false);
+  };
+
+  return (
+    <Container>
+      <form>
+        <TextField
+          type="text"
+          label="Username"
+          onChange={(e) => {
+            setUserName(e.target.value);
+          }}
+        ></TextField>
+        <br />
+        <TextField
+          type="text"
+          label="Password"
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+        ></TextField>
+        <br />
+
+        <Button
+          variant="outlined"
+          color="primary"
+          type="submit"
+          onClick={(e) => {
+            e.preventDefault()
+            // if(loginSuccess === true){
+            //     window.location.replace("http://localhost:3000");
+                
+            // }
+            
+            Axios.post("/api/login", {
               user_name: userName,
-              password: password
-              }).then((res) => {console.log(res)})
-        }} 
-        style ={{marginTop: "10%"}}>
-  Log In
-</Button>
-        </form>
+              password: password,
+            }).then((res) => {
+            setOpen(true)
+            if(res.data.message !== "User Does Not exist" && res.data.message !== "Wrong Username or Password" ) {
+              setLoginSuccess(true)
+              console.log(res);
+              window.location.replace("/");
+            }
+            });
+          }}
+          style={{ marginTop: "10%" }}
+        >
+          Log In
+        </Button>
+      </form>
 
-        </Container>
-)
+      {open === true ? (
+          loginSuccess === true ?
+        (<Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+          <Alert onClose={handleClose} severity="success">
+            Successfuly Loged In!
+          </Alert>
+        </Snackbar>)
+        : 
+        (<Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+          <Alert onClose={handleClose} severity="error">
+            Username/Password is not correct
+          </Alert>
+        </Snackbar>)
+      ) : null}
+    </Container>
+  );
 }
